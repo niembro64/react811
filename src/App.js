59a28e30faee1f1c01c16494970388ae0547e4b9
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-14 11:27:56 -0400

    array of tags

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
 function App() {
+    const [places, setPlaces] = useState([
+        <li>"Mason's Creamery</li>,
+        <li>Mitchell's Ice Cream</li>,
+        <li>Margie's Candies</li>,
+    ]);
+
     return (
         <div className="App">
             <h1>Favorite Ice Cream Places</h1>
-            
+
+            <ul>{places}</ul>
         </div>
     );
 }
